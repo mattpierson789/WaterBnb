dd0725b23f6ef4ddd951a821f0f6584c6923ec19
Matt Pierson <matt.pierson789@gmail.com>
2023-06-06 18:36:43 -0400

    review controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,54 +1,47 @@
-# modul
-
-# def create 
-
-#     @review = Review.new(review_params)
-
-#     if @review.save 
-#         render :show 
-#     else 
-#         render json: @review.errors.full_messages, status: 422
-#     end
-
-# end 
-
-# def index 
-
-#     @reviews = Review.all.where(listing_id: params[:listing_id])
-#     render :index
-
-# end 
-
-# def show 
-
-#     @review = Review.find(params[:id])
-#     render :show
-
-# end 
-
-# def destroy 
-
-#     @review = Review.find(params[:id])
-#     @review.destroy
-#     render :show
-
-# end 
-
-
-# def update 
-
-#     @review = Review.find(params[:id])
-
-#     if @review.update(review_params)
-#         render :show
-#     else
-#         render json: @review.errors.full_messages, status: 422
-#     end
-
-# end 
-
-# private 
-
-# def review_params 
-#     params.require(:review).permit(:reviewer_id, :listing_id, :rating, :body, :cleanliness, :communication, :check_in, :accuracy, :location, :value)
-# end
+module Api
+    class ReviewsController < ApplicationController
+  
+      def create 
+        @review = Review.new(review_params)
+  
+        if @review.save 
+          render :show 
+        else 
+          render json: @review.errors.full_messages, status: 422
+        end
+      end 
+  
+      def index 
+        @reviews = Review.where(listing_id: params[:listing_id])
+        render :index
+      end 
+  
+      def show 
+        @review = Review.find(params[:id])
+        render :show
+      end 
+  
+      def destroy 
+        @review = Review.find(params[:id])
+        @review.destroy
+        render :show
+      end 
+  
+      def update 
+        @review = Review.find(params[:id])
+  
+        if @review.update(review_params)
+          render :show
+        else
+          render json: @review.errors.full_messages, status: 422
+        end
+      end 
+  
+      private 
+  
+      def review_params 
+        params.require(:review).permit(:reviewer_id, :listing_id, :rating, :body, :cleanliness, :communication, :check_in, :accuracy, :location, :value)
+      end
+    end
+  end
+  

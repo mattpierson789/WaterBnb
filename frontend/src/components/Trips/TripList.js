343d86bb578800d6a047362be30ede4177dab06e
Matt Pierson <matt.pierson789@gmail.com>
2023-09-10 16:18:24 -0400

    message

--- frontend/src/components/Trips/TripList.js
+++ frontend/src/components/Trips/TripList.js
@@ -1,47 +1,93 @@
+// import TripItem from "./TripItem";
+// import "./TripList.css";
+// import empty from "../../assets/images/empty.jpg";
+
+// const TripsList = ({ trips, type }) => {
+//   let header;
+//   switch (type) {
+//     case "past":
+//       header = "Where you've been";
+//       break;
+//     case "current":
+//       header = "Your current trips";
+//       break;
+//     case "future":
+//       header = "Where you're going next !";
+//       break;
+//     default:
+//       header = "Booked trips";
+//   }
+
+//   const tripItems = trips.map((trip, i) => (
+//     <div className="trip-item-container" key={trip.id}>
+//       <TripItem trip={trip} type={type} />
+//     </div>
+//   ));
+
+//   const emptyList =
+//     <div className="empty-list">
+//       <img src={empty} alt="empty-list-gif" />
+//       <div>No Trips ...  get to booking!</div>
+//     </div>
+
+//   return (
+//     <div className="trip-list-container">
+//       <div className="trip-list-header">
+//         <h2>{header}</h2>
+//       </div>
+//       {trips.length === 0 ? (
+//         emptyList
+//       ) : (
+//         <div className="trip-list-grid">{tripItems}</div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default TripsList;
+
+
 import TripItem from "./TripItem";
 import "./TripList.css";
 import empty from "../../assets/images/empty.jpg";
 
 const TripsList = ({ trips, type }) => {
-  let header;
-  switch (type) {
-    case "past":
-      header = "Where you've been";
-      break;
-    case "current":
-      header = "Your current trips";
-      break;
-    case "future":
-      header = "Where you're going next !";
-      break;
-    default:
-      header = "Booked trips";
-  }
+    let header;
+    switch (type) {
+        case "past":
+            header = "Where you've been";
+            break;
+        case "whereYouAreGoing":
+            header = "Where you are going";
+            break;
+        default:
+            header = "Booked trips";
+    }
 
-  const tripItems = trips.map((trip, i) => (
-    <div className="trip-item-container" key={trip.id}>
-      <TripItem trip={trip} type={type} />
-    </div>
-  ));
+    const tripItems = trips.map((trip, i) => (
+        <div className="trip-item-container" key={trip.id}>
+            <TripItem trip={trip} type={type} />
+        </div>
+    ));
 
-  const emptyList =
-    <div className="empty-list">
-      <img src={empty} alt="empty-list-gif" />
-      <div>It's empty here ... nothing to see</div>
-    </div>
+    const emptyList = 
+        <div className="empty-list">
+            <img src={empty} alt="empty-list-gif" />
+            <div>No Trips ...  get to booking!</div>
+        </div>;
 
-  return (
-    <div className="trip-list-container">
-      <div className="trip-list-header">
-        <h2>{header}</h2>
-      </div>
-      {trips.length === 0 ? (
-        emptyList
-      ) : (
-        <div className="trip-list-grid">{tripItems}</div>
-      )}
-    </div>
-  );
+    return (
+        <div className="trip-list-container">
+            <div className="trip-list-header">
+                <h2>{header}</h2>
+            </div>
+            {trips.length === 0 ? (
+                emptyList
+            ) : (
+                <div className="trip-list-grid">{tripItems}</div>
+            )}
+        </div>
+    );
 };
 
 export default TripsList;
